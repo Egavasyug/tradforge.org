7d3288925e84640d33f3be6a46c24b2fc585fdd2
Dr. Savage <78974706+Egavasyug@users.noreply.github.com>
2025-09-06 19:35:20 -0400

    chore(favicon): use tradforge-logo.png as favicon and apple-touch-icon globally

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import '@/styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="min-h-screen">
+      <Head>
+        {/* Use the site logo as the favicon (PNG works across modern browsers) */}
+        <link rel="icon" href="/tradforge-logo.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/tradforge-logo.png" />
+      </Head>
       <Component {...pageProps} />
     </div>
   );
