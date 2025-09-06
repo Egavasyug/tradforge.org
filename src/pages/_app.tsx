eed4418e85ed45631a8c7629c6da433d6ccc26f0
Dr. Savage <78974706+Egavasyug@users.noreply.github.com>
2025-09-06 19:43:39 -0400

    chore(favicon): also include tradforge-log.png as icon to support uploaded filename

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,8 +6,9 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="min-h-screen">
       <Head>
-        {/* Use the site logo as the favicon (PNG works across modern browsers) */}
+        {/* Use the site logo as the favicon (PNG works across modern browsers). Include both filenames for robustness. */}
         <link rel="icon" href="/tradforge-logo.png" type="image/png" />
+        <link rel="icon" href="/tradforge-log.png" type="image/png" />
         <link rel="apple-touch-icon" href="/tradforge-logo.png" />
       </Head>
       <Component {...pageProps} />
