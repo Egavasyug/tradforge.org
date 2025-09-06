917f59dc2ca71cc3b5609b90b4aac4b97149d582
Dr. Savage <78974706+Egavasyug@users.noreply.github.com>
2025-09-06 16:52:01 -0400

    style: switch global background to light pink (rose-50)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,8 @@ import '@/styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="min-h-screen bg-blue-50">
+    <div className="min-h-screen bg-rose-50">
       <Component {...pageProps} />
     </div>
   );
 }
-
